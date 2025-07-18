a2c31577b8a3e253ef25a0fd8c9b6a6aef0f396a
PrabhatiJena <jenaprabhati97@gmail.com>
2025-07-18 09:58:37 +0530

    updated

--- src/main/java/com/example/crud_operation/CrudOperationApplication.java
+++ src/main/java/com/example/crud_operation/CrudOperationApplication.java
@@ -8,16 +8,7 @@ public class CrudOperationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudOperationApplication.class, args);
-                int a = 10;
-                int j = 20;
-                doubel d = 50.
-                String name = "Prabhati";
-                int c = 40
-                doubel d = 50.00;
-                String name = "Prabhati";
-		boolean status = false;
-                int r = 70;
-
+                
 
 	}
 
